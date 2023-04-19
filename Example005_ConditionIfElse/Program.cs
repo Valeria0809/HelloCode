2aa6e099221e02db66fdfe864146acfbd8e4ca56
VALERIA <valeri.tereshenkova@icloud.com>
2023-04-19 22:54:46 +0300

    Example 005 ConditionIfElse project

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write ("Введите ваше имя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "иван")
+{
+    Console.WriteLine ("Ура, это же ИВАН!");
+}
+else
+{
+    Console.Write ("Привет, ");
+    Console.WriteLine (username);
+}
